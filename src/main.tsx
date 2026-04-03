6c6f3062232ccfe80de17b257d0dadd8792bb1d3
HamalainenTaru <thamalainen1990@gmail.com>
2026-04-03 17:48:10 +0300

    .

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,7 @@ import { HashRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <HashRouter basename="/questify-app">
+    <HashRouter>
       <App />
     </HashRouter>
   </StrictMode>,
